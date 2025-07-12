58d467abe3a2c653388f07fc1f07ff4742062622
RJaimeV <156475750+RJaimeV@users.noreply.github.com>
2025-07-11 18:57:00 -0600

    Coins_added

---
+++ Assets/Scripts/CoinsUI.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class CoinsUI : MonoBehaviour
+{
+    [SerializeField]
+    private Text _coinsText;
+    [SerializeField]
+    private UnityEvent _OnCoinsUpdated;
+    public void UpodateCoins(int coinsNumber)
+    {
+        _coinsText.text = "x" + coinsNumber;
+        _OnCoinsUpdated.Invoke();
+    }
+}
